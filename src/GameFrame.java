acc5112353a2dda1c0ed7257eec43de4fb867180
unknown <manvirm29@gmail.com>
2022-01-03 23:21:25 -0800

    add frame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,5 +1,21 @@
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
-
+	
+	
+	GameFrame(){
+		
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		//takes JFrame and fits it around components we add in frame
+		this.pack();
+		this.setVisible(true);
+		//have window appear in middle of screen
+		this.setLocationRelativeTo(null);
+	
+		
+	}
 }
