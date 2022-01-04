d4ccc73caff7836c73fae796234e3aa0b04dace4
unknown <manvirm29@gmail.com>
2022-01-03 22:59:52 -0800

    initialize files

---
+++ src/GamePanel.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
