4e13206b093223099c3e5e15452a59e7806136e3
unknown <manvirm29@gmail.com>
2022-01-04 17:15:19 -0800

    fix title

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -6,7 +6,7 @@ public class GameFrame extends JFrame{
 	GameFrame(){
 		
 		this.add(new GamePanel());
-		this.setTitle("Snake");
+		this.setTitle("Snake Game");
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
