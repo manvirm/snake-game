86f72ccbc8031b058d2c4ba4affafcd13c68fe53
unknown <manvirm29@gmail.com>
2022-01-06 20:16:14 -0800

    add snake movement

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -175,7 +175,31 @@ public class GamePanel extends JPanel implements ActionListener {
 	public class MyKeyAdapter extends KeyAdapter{
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			//avoid user from turning 180 degrees in opposite direction
+			//which results in self collision (game over)
+			//limit user to 90 degree turns to avoid this
+			switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction != 'R') {
+					direction = 'L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT:
+				if(direction != 'L') {
+					direction = 'R';
+				}
+				break;
+			case KeyEvent.VK_UP:
+				if(direction != 'D') {
+					direction = 'U';
+				}
+				break;
+			case KeyEvent.VK_DOWN:
+				if(direction != 'U') {
+					direction = 'D';
+				}
+				break;
+			}
 		}
 	}
 
