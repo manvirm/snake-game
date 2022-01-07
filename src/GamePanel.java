db421d642d9ef4009426abe173371b980032194e
unknown <manvirm29@gmail.com>
2022-01-06 20:07:59 -0800

    handle collisions

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -28,7 +28,8 @@ public class GamePanel extends JPanel implements ActionListener {
 	int appleX;
 	int appleY;
 	
-	char direction = 'U';
+	//default direction the snake will move
+	char direction = 'D';
 	boolean running = false;
 	Timer timer;
 	Random random;
@@ -70,6 +71,22 @@ public class GamePanel extends JPanel implements ActionListener {
 		//create circle (apple)
 		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 		
+		//draw all body parts of snake (head, body)
+		//iterate through all body parts
+		for(int i = 0; i < bodyParts; i++) {
+			
+			//draw head
+			if(i == 0) {
+				g.setColor(Color.green);
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			}
+			//draw body
+			else {
+				g.setColor(new Color(45, 180, 0));
+				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+			}
+		}
+		
 	}
 	public void newApple() {
 		//have apple appear somewhere along x axis
@@ -87,7 +104,7 @@ public class GamePanel extends JPanel implements ActionListener {
 			
 		}
 		//directions for up, down, left, right in that order
-		// change coordinates and break
+		//change coordinates and break
 		switch(direction) {
 		case 'U':
 			y[0] = y[0] - UNIT_SIZE;
@@ -103,11 +120,38 @@ public class GamePanel extends JPanel implements ActionListener {
 			break;
 		}
 	}
+	
 	//snake eats apples to score points
 	public void checkApple() {
 		
 	}
 	public void checkCollisions() {
+		
+		//check if head collided with body, then set game over
+		for(int i = bodyParts; i > 0; i--) {
+			if((x[0] == x[i]) && (y[0] == y[i])) {
+				running = false;
+			}
+		}
+		//check if head touches left border
+		if(x[0] < 0) {
+			running = false;
+		}
+		//check if head touches right border
+		if(x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+		//check if head touches top border
+		if(y[0] < 0) {
+			running = false;
+		}
+		//check if head touches bottom border
+		if(y[0] > SCREEN_HEIGHT) {
+			running = false;
+		}
+		if(!running) {
+			timer.stop();
+		}
 		
 	}
 	public void gameOver(Graphics g) {
@@ -115,7 +159,16 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		
+		//if game is running, move snake, check to see if we ran in apple
+		// and check for collisions
+		if(running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		//if game is no longer running repaint (redraws components)
+		repaint();
 		
 	}
 	
